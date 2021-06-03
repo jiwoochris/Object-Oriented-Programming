bb4012ec589a7c500a68fb3ead72ffdc0b15818f
juicy_jung <83687471+juicyjung@users.noreply.github.com>
2021-06-04 02:24:04 +0900

    Add files via upload

---
+++ Ex14_14.java
@@ -0,0 +1,14 @@
+package myPackage;
+
+public class Ex14_14 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		LinkedQueue l = new LinkedQueue();
+
+	}
+	
+	
+
+}
